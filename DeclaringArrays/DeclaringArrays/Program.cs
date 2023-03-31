62e4609df0c75a28140c3eab463cfc1e6212e5ff
Aaron Drury <83255715+adrury0726@users.noreply.github.com>
2023-03-31 15:47:30 -0600

    Add files via upload

---
+++ DeclaringArrays/DeclaringArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace DeclaringArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare and initialize array grades
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 12;
+            grades[3] = 9;
+            grades[4] = 7;
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+            string input = Console.ReadLine();
+            // assign value to array grades at index 0
+            grades[0] = int.Parse(input);
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+            Console.ReadKey();
+
+            //Another way to initialize an array
+            int[] gradesOfMathStudentsA = { 20, 13, 12, 8, 8 };
+
+            //Another way to initialze an array
+            int[] gradesOfMathStudentsB = new int[] { 15, 20, 3, 17, 18, 15 };
+
+            Console.WriteLine("Length of gradesOfMathStudentsA: {0}" , gradesOfMathStudentsA.Length);
+            Console.ReadKey();
+
+        }
+    }
+}
