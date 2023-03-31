812cd766076d20e15cb4984b9b9c48f19c485a7a
Aaron Drury <83255715+adrury0726@users.noreply.github.com>
2023-03-31 15:49:46 -0600

    Add files via upload

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 50; counter+=5)
+            {
+                Console.WriteLine(counter);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from a method.");
+            Console.Read();
+        }
+        // access modifier (static), then return type method name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+    }
+}
