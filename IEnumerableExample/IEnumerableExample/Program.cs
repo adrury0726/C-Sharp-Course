e306305c231d919c4692bb18593fe9cdc866965d
Aaron Drury <83255715+adrury0726@users.noreply.github.com>
2023-04-05 09:17:00 -0600

    Add files via upload

---
+++ IEnumerableExample/IEnumerableExample/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace IEnumerableExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create IEnumerable variable that takes int collection type
+            IEnumerable<int> unknownCollection;
+            unknownCollection = GetCollection(1);
+
+            Console.WriteLine("This was a List<int>");
+            foreach(int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+
+            //New Line
+            Console.WriteLine(" ");
+
+            //Call GetCollection() with option = 2 which will return a Queue<int>
+            unknownCollection = GetCollection(2);
+
+            Console.WriteLine("This was a Queue<int>");
+            //for each number in the collection we got back from GetCollection(2)
+            foreach(int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+
+            //This will return our else collection
+            unknownCollection = GetCollection(5);
+
+            Console.WriteLine("This was a Queue<int>");
+            //for each number in the collection we got back from GetCollection(5)
+            foreach (int num in unknownCollection)
+            {
+                Console.Write(num + " ");
+            }
+
+        }
+
+        static IEnumerable<int> GetCollection(int option)
+        {
+            //Create a list of numbers and initialize it
+            List<int> numbersList = new List<int>() { 1, 2, 3, 4, 5 };
+
+            Queue<int> numbersQueue = new Queue<int>();
+            //add values to the queue
+            numbersQueue.Enqueue(6);
+            numbersQueue.Enqueue(7);
+            numbersQueue.Enqueue(8);
+            numbersQueue.Enqueue(9);
+            numbersQueue.Enqueue(10);
+
+            //if option is 1
+            if (option == 1)
+            {
+                //return the list of type List<int>
+                return numbersList;
+            }
+            else if (option == 2)
+            {
+                return numbersQueue;
+            }
+            else
+            {
+                return new int[] { 11, 12, 13, 14, 15 };
+            }
+        }
+    }
+}
---
+++ IEnumerableExample2/IEnumerableExample2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IEnumerableExample2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List of type List<int> initialized
+            List<int> numberList = new List<int>() { 8, 6, 2};
+            //Array of type int[] initialized
+            int[] numberArray = new int[] { 1, 7, 1, 3 };
+            //new line
+            Console.WriteLine(" ");
+            //Call CollectionSum() and pass the list to do it
+            CollectionSum(numberList);
+
+            Console.WriteLine(" ");
+            //Call CollectionSum() and pass the list to do it
+            CollectionSum(numberArray);
+        }
+
+        static void CollectionSum(IEnumerable<int> anyCollection)
+        {
+            int sum = 0;
+
+            foreach (int num in anyCollection)
+            {
+                //add the num value to the sum
+                sum += num;
+            }
+
+            Console.Write("Sum is {0}", sum);
+        }
+    }
+}
