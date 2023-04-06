eea1a28aa6682cfe165020866508ada98636a3d5
Aaron Drury <83255715+adrury0726@users.noreply.github.com>
2023-04-06 08:15:33 -0600

    Add files via upload

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Enums
+{
+    enum Day { Mo, Tu, We, Th, Fr, Sa, Su};
+    enum Month { Jan = 1, Feb, Mar, Apr, May, Jun, Jul = 12, Aug, Sep, Oct, Nov, Dec};
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day Su = Day.Su;
+
+            Day a = Day.Fr;
+
+            Console.WriteLine(fr == a);
+
+            Console.WriteLine(Day.Mo);
+            Console.WriteLine((int)Day.Mo);
+
+            Console.WriteLine((int)Month.Feb);
+            Console.WriteLine((int)Month.Jul);
+            Console.WriteLine((int)Month.Aug);
+
+            Console.ReadKey();
+        }
+    }
+}
